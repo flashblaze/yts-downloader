fd7edca33a0f818cc539541fcb0b692626294bc0
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-04-28 22:48:03 +0530

    Downloading hardcoded movie title torrent

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import requests
 
-r = requests.get('https://yts.am/api/v2/list_movies.json?limit=2')
+params = {
+    'limit': 20,
+    'page': 1
+}
+
+r = requests.get('https://yts.am/api/v2/list_movies.json', data=params)
 r_dict = r.json()
-movieURL = r_dict['data']['movies'][0]['torrents'][0]['url']
-movieTitle = r_dict['data']['movies'][0]['title']
-with open(movieTitle + '.torrent', 'wb') as f:
-    f.write(requests.get(movieURL).content)
+urls = []
+titles = []
+for i in range(params['limit']):
+    urls.append(r_dict['data']['movies'][i]['torrents'][0]['url'])
+    titles.append(r_dict['data']['movies'][i]['title'])
+
+name = 'the trip to spain'.lower()
+
+print(titles)
+count = 0
+val = 0
+for title in titles:
+    if name in title.lower():
+        print('yes')
+        val = count
+    else:
+        count += 1
+
+finalURL = urls[val]
+finalTitle = titles[val]
+
+# movieTitle = r_dict['data']['movies'][0]['title']
+print(finalTitle)
+with open(finalTitle + '.torrent', 'wb') as f:
+    f.write(requests.get(finalURL).content)
