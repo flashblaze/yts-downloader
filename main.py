9c23df8bcc4f63000abb9bddfc6b7120b6aef610
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-04-29 10:14:22 +0530

    Downloads first url of the torrent, if the title is available

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import requests
 import sys
 
-# Parameters to be added at the end of the url
-params = {
-    'limit': 20,
-    'page': 1
-}
-r = requests.get('https://yts.am/api/v2/list_movies.json', data=params)
-
-# Using json method, since the response is in json
-r_dict = r.json()
-urls = []
-titles = []
-
-for i in range(params['limit']):
-    urls.append(r_dict['data']['movies'][i]['torrents'][0]['url'])
-    titles.append(r_dict['data']['movies'][i]['title'])
 
 """
 Parsing the 2nd argument which is the movie title
@@ -26,24 +11,46 @@ In the above case, arg[1],  which is 'The Trip To Spain', will be stored in name
 """
 name = sys.argv[1].lower()
 
-print(titles)
-count = 0
-val = 0
-flag = 0
-for title in titles:
-    if name in title.lower():
-        print('Title is available!')
-        val = count
-        flag = 1
+urls = []
+titles = []
+page = 1
+
+while True and page < 500:
+
+    # Parameters to be added at the end of the url
+    payload = {
+        'limit': 50,
+        'page': page
+    }
+    r = requests.get('https://yts.am/api/v2/list_movies.json', params=payload)
+    print(r.url)
+    # Using json method, since the response is in json
+    r_dict = r.json()
+
+    for i in range(payload['limit']):
+        urls.append(r_dict['data']['movies'][i]['torrents'][0]['url'])
+        titles.append(r_dict['data']['movies'][i]['title'])
+
+    count = 0
+    val = 0
+    flag = 0
+    for title in titles:
+        if name in title.lower():
+            print('Title is available!')
+            val = count
+            flag = 1
+        else:
+            count += 1
+
+    if(flag == 0):
+        pass
     else:
-        count += 1
+        finalURL = urls[val]
+        finalTitle = titles[val].replace(':', '')
+        with open(finalTitle + '.torrent', 'wb') as f:
+            f.write(requests.get(finalURL).content)
+        exit()
 
-if(flag == 0):
-    print("Title not found. Try again!")
-else:
-    finalURL = urls[val]
-    finalTitle = titles[val]
-
-    # movieTitle = r_dict['data']['movies'][0]['title']
-    with open(finalTitle + '.torrent', 'wb') as f:
-        f.write(requests.get(finalURL).content)
+    page += 1
+    payload['page'] = page
+    r_dict.clear()
